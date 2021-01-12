65e13386333bd15abae1997a533c3e3161a4124e
Tommy Giardina <tommy.giardina@gmail.com>
2021-01-11 19:09:09 -0600

    Updated await script

--- scripts/await-elastic.sh
+++ scripts/await-elastic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 RETRIES=20
-until curl http://localhost:9200 > /dev/null 2>&1 || [ $RETRIES -eq 0 ];
+until curl http://elasticsearch:9200 > /dev/null 2>&1 || [ $RETRIES -eq 0 ];
 do
   echo "Waiting for elasticsearch server, $((RETRIES--)) remaining attempts..."
   sleep 1
